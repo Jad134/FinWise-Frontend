a7c57778cb1379f92fb17fceb39d74e420545bde
unknown <jadelnader99@gmail.com>
2025-01-24 20:29:00 +0100

    start create intro

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { MainContentComponent } from './main-content/main-content.component';
 import { HomeComponent } from './main-content/home/home.component';
 import { LoginComponent } from './authentication/login/login.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 export const routes: Routes = [
     {
@@ -10,5 +11,9 @@ export const routes: Routes = [
         children:[
             { path: 'home', component: HomeComponent },
         ]
+    },
+    {
+        path: '',
+        component: LandingPageComponent,
     },
 ];
