e6f986dd5e78b18de2448f4116f5b08a4cf6a800
unknown <jadelnader99@gmail.com>
2025-01-24 18:54:50 +0100

    start create routes and child routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,14 @@
 import { Routes } from '@angular/router';
 import { MainContentComponent } from './main-content/main-content.component';
+import { HomeComponent } from './main-content/home/home.component';
+import { LoginComponent } from './authentication/login/login.component';
 
 export const routes: Routes = [
-    {path: '', component: MainContentComponent},
+    {
+        path: 'dashboard',
+        component: MainContentComponent,
+        children:[
+            { path: 'home', component: HomeComponent },
+        ]
+    },
 ];
