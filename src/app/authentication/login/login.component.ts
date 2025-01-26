946c52a149f325beb6e9298dc679151a5667e174
unknown <jadelnader99@gmail.com>
2025-01-26 17:56:10 +0100

    login-page inputs

--- src/app/authentication/login/login.component.ts
+++ src/app/authentication/login/login.component.ts
@@ -1,13 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { MainBackgroundComponent } from "../../shared/main-background/main-background.component";
 
 @Component({
-  selector: 'app-login',
-  standalone: true,
-  imports: [RouterModule],
-  templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+    selector: 'app-login',
+    standalone: true,
+    templateUrl: './login.component.html',
+    styleUrl: './login.component.scss',
+    imports: [RouterModule, MainBackgroundComponent]
 })
 export class LoginComponent {
+  passwordVisible = false;
 
+  togglePasswordVisibility(): void {
+    this.passwordVisible = !this.passwordVisible;
+  }
 }
---
+++ src/app/shared/main-background/main-background.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input  } from '@angular/core';
+
+@Component({
+  selector: 'app-main-background',
+  standalone: true,
+  imports: [],
+  templateUrl: './main-background.component.html',
+  styleUrl: './main-background.component.scss'
+})
+export class MainBackgroundComponent {
+  @Input() headline: string = 'Headline'; // Standardtext für den Titel
+}
