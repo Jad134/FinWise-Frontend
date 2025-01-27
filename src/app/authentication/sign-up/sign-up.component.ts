257c52dba5013051f606f8b6b3be66e19b5958cc
unknown <jadelnader99@gmail.com>
2025-01-27 19:15:41 +0100

    toggle passwords

--- src/app/authentication/sign-up/sign-up.component.ts
+++ src/app/authentication/sign-up/sign-up.component.ts
@@ -2,16 +2,21 @@ import { Component } from '@angular/core';
 import { MainBackgroundComponent } from "../../shared/main-background/main-background.component";
 
 @Component({
-    selector: 'app-sign-up',
-    standalone: true,
-    templateUrl: './sign-up.component.html',
-    styleUrl: './sign-up.component.scss',
-    imports: [MainBackgroundComponent]
+  selector: 'app-sign-up',
+  standalone: true,
+  templateUrl: './sign-up.component.html',
+  styleUrl: './sign-up.component.scss',
+  imports: [MainBackgroundComponent]
 })
 export class SignUpComponent {
   passwordVisible = false;
+  confirmedPasswordVisisble = false;
 
   togglePasswordVisibility(): void {
     this.passwordVisible = !this.passwordVisible;
+  }
+
+  toggleConfirmedPasswordVisibiliy(): void {
+    this.confirmedPasswordVisisble = !this.confirmedPasswordVisisble;
   }
 }
