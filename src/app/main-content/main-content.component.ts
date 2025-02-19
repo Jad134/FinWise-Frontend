d8d25c3eedf06862282768fed343df0105539fd7
unknown <jadelnader99@gmail.com>
2025-02-19 09:32:21 +0100

    start design mainpage

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { DashboardBackgroundComponent } from "../shared/dashboard-background/dashboard-background.component";
 
 @Component({
-  selector: 'app-main-content',
-  standalone: true,
-  imports: [RouterModule],
-  templateUrl: './main-content.component.html',
-  styleUrl: './main-content.component.scss'
+    selector: 'app-main-content',
+    standalone: true,
+    templateUrl: './main-content.component.html',
+    styleUrl: './main-content.component.scss',
+    imports: [RouterModule, DashboardBackgroundComponent]
 })
 export class MainContentComponent {
 
