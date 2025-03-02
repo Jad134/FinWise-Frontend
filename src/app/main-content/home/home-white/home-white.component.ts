b2bcf7f5639e6f1a3e83dca5febbd64f7414108e
unknown <jadelnader99@gmail.com>
2025-03-02 11:17:28 +0100

    start circle progress bar

--- src/app/main-content/home/home-white/home-white.component.ts
+++ src/app/main-content/home/home-white/home-white.component.ts
@@ -9,4 +9,10 @@ import { Component } from '@angular/core';
 })
 export class HomeWhiteComponent {
 
+  progressPercentage: number = 75; // Beispielwert
+  circumference: number = 2 * Math.PI * 45; // Kreisumfang für r=45
+
+  get progressOffset(): number {
+    return this.circumference * (1 - this.progressPercentage / 100);
+  }
 }
