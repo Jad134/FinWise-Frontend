0ce59f26fa2a192262eef1817466e2d7bc8a15b4
unknown <jadelnader99@gmail.com>
2025-03-09 11:15:22 +0100

    add link to navbar and change navbar icon color with link dependent

--- src/app/shared/dashboard-background/dashboard-background.component.ts
+++ src/app/shared/dashboard-background/dashboard-background.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard-background',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './dashboard-background.component.html',
   styleUrl: './dashboard-background.component.scss'
 })
 export class DashboardBackgroundComponent {
 
+  currentRoute: string = ''
+
+
+  constructor(private router: Router) {
+    this.router.events.subscribe(() => {
+      this.currentRoute = this.router.url;
+    });
+  }
 }
