8ba946655cbaed223d8cdb26282fb67887c09b67
unknown <jadelnader99@gmail.com>
2025-03-14 15:40:00 +0100

    add income and expense period filter for overview

--- src/app/main-content/analysis/analysis-white/analysis-white.component.ts
+++ src/app/main-content/analysis/analysis-white/analysis-white.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { BarChartComponent } from "../../../shared/components/bar-chart/bar-chart.component";
+import { FinanceService } from '../../../services/shared-functions.service';
 
 @Component({
     selector: 'app-analysis-white',
@@ -10,4 +11,34 @@ import { BarChartComponent } from "../../../shared/components/bar-chart/bar-char
 })
 export class AnalysisWhiteComponent {
 
+    constructor() {
+        this.fetchData('daily')
+    }
+
+
+    sharedFunctions = inject(FinanceService)
+    selectedPeriod: string = 'daily';
+    totalIncome: number = 0;
+    totalExpenses: number = 0;
+
+
+    fetchData(period: string) {
+        this.selectedPeriod = period;
+        this.getIncome(period)
+        this.getExpense(period)
+    }
+
+
+    getIncome(period: any) {
+        this.sharedFunctions.getIncome(period).subscribe(data => {
+            this.totalIncome = data.reduce((sum: number, income: any) => sum + parseFloat(income.amount), 0);
+        });
+    }
+
+
+    getExpense(period: any) {
+        this.sharedFunctions.getExpenses(period).subscribe(data => {
+            this.totalExpenses = data.reduce((sum: number, expense: any) => sum + parseFloat(expense.amount), 0);
+        });
+    }
 }
--- src/app/services/api-urls.service.ts
+++ src/app/services/api-urls.service.ts
@@ -9,6 +9,8 @@ export class ApiUrlsService {
 
   BASE_URL = 'http://127.0.0.1:8000/api';
   REGISTER_URL = `${this.BASE_URL}/registration/`;
-  LOGIN_URL = `${this.BASE_URL}/login/`
-  TOTAL_BALANCE_URL = `${this.BASE_URL}/overview/`
+  LOGIN_URL = `${this.BASE_URL}/login/`;
+  TOTAL_BALANCE_URL = `${this.BASE_URL}/overview/`;
+  INCOME_AMOUNT_URL = `${this.BASE_URL}/income/`;
+  EXPENSE_AMOUNT_URL = `${this.BASE_URL}/expense/`
 }
