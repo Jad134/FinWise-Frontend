dce0bf14de9bb23cb35b502379db2c9a27bc8a7c
unknown <jadelnader99@gmail.com>
2025-03-20 19:15:11 +0100

    add Total Balance

--- src/app/main-content/transaction/transaction-green/transaction-green.component.ts
+++ src/app/main-content/transaction/transaction-green/transaction-green.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
+import { FinanceService } from '../../../services/shared-functions.service';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-transaction-green',
@@ -11,10 +13,26 @@ import { Router, RouterModule } from '@angular/router';
 export class TransactionGreenComponent {
 
   constructor(private router: Router) {
+    this.getTotalBalance()
   }
 
+  totalBalance: number = 0;
+  sharedFunctionService = inject(FinanceService)
+
+  private totalBalance$ = new BehaviorSubject<number | undefined>(undefined);
 
   routeToHome() {
     this.router.navigate(['dashboard/home'])
+  }
+
+
+  /**
+   * Get the amount of the Total Balance
+   */
+  getTotalBalance() {
+    this.sharedFunctionService.getTotalBalance().subscribe(balance => {
+      this.totalBalance = balance;
+      this.totalBalance$.next(balance);
+    });
   }
 }
