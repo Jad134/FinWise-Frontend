aed5a583baee66869adf19c82083152260338d70
unknown <jadelnader99@gmail.com>
2025-03-27 18:14:01 +0100

    bugfix white font from primeng with setting primeng selector in app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,28 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideHttpClient(),]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideHttpClient(),
+    provideAnimationsAsync(),  // Um Animationen asynchron zu laden
+    providePrimeNG({ 
+      theme: {
+        preset: Aura ,
+        options: {
+          darkModeSelector: '.my-app-dark', // Dark Mode durch Toggle
+          cssLayer: {
+              name: 'primeng',
+              order: 'app-styles, primeng, another-css-library'
+          }
+      } 
+      }
+    })
+  ]
 };
